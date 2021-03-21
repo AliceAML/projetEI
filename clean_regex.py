232a15cf41dcdefd8d963504c8ba88e1b974b49b
Hortatori <53857735+Hortatori@users.noreply.github.com>
2021-03-21 15:22:54 +0100

    Update clean_regex.py

--- clean_regex.py
+++ clean_regex.py
@@ -1,5 +1,4 @@
 import regex as re
-# data =  open("corpus_maison/Causette-84-bd-Ecriture inclusive.txt", "r", encoding="utf-8")
 name = "corpus_maison/Causette-84-bd-Ecriture inclusive.txt"
 def load_file(filename):
     data = open(filename, 'r', encoding="utf-8")
@@ -13,33 +12,31 @@ def load_file(filename):
 
 
 text = load_file(name)
-# sentences = re.split(r"[\s]+[\.?!]", text)
-# sentences = re.split(r"\s", text)
-# sentences = re.split(r"\s", text)
-# sentences = re.split(r"\s", text)
-# sentences = re.split(r"(\.|(\s+(\?|!))\s)", text)
-# sentences = re.findall(r'(?:\d[,.]|[^,.])*(?:[,.]|$)', text)
 sentences = re.split(r"(?<=\.|\?|\!)\s", text)
 
 print("text 100",text[0:100])
-# à propos de la regex : \b pour check les fins de mots dans une string (il faut qu'il y ai
+# à propos de la regex : \b pour identifier les fins de mots dans une string (utilisable sur une pharse, il faut qu'il y ai
 # un séparateur. si on passe sur des items : pas de sep)
 print("sentences 10", sentences[:10])
-out = []
-for one_sentence in sentences : 
-    tempwords = one_sentence.split(' ')
-    tempgold = []
-    for word in tempwords :
-        if re.search('·', word):
-            tempgold.append(True)
-        else :
-            tempgold.append(False)
-    tempdict = dict()
-    tempdict['words'] = tempwords
-    tempdict['gold'] = tempgold
-    out.append(tempdict)
+def labelling(sentences) :
+    out = []
+    for one_sentence in sentences : 
+        tempwords = one_sentence.split(' ')
+        tempgold = []
+        for index, word in enumerate(tempwords) :
+            if re.search('·', word):
+                tempgold.append(index)
+        tempdict = dict()
+        tempdict['words'] = tempwords
+        tempdict['gold'] = tempgold
+        out.append(tempdict)
+    return out
 
-print(out)
+labels = labelling(sentences)
+for i in labels :
+    if i['gold'] :
+        for j in i['gold'] :
+            print(i['words'][j])
           
 
 
@@ -47,7 +44,8 @@ print(out)
 # output = list(map(lambda x: x.replace('\t','').replace('\n',''),text))
 
 # 1  nettoyer le texte (tabulation? passages de lignes?)
-#     phrases coupees par des sauts de lignes
+#       phrases coupees par des sauts de lignes = ok
+#       phrases de titres : elles n'ont pas de points, ça les fusionne
 # 2  mettre en forme de corpus (par textes entiers?)
 # 3  estimer les meilleures regex
 
