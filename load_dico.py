0527338633d4429b1ba7bb5f649652f71c0b1bc5
AliceAML <alice.hammel@gmail.com>
2021-05-07 11:35:53 +0200

    remplace set par trie pour calcul distane

--- load_dico.py
+++ load_dico.py
@@ -1,19 +1,21 @@
-"""Script that loads a set of French words,
+"""Script that loads a trie of French words,
 to be used in other scripts"""
 
 import os
 import logging
+from pytrie import StringTrie
 
 path = "../French-Dictionary-master/dictionary"
 
 files = os.listdir(path)
 
-dico = set()
+dico = StringTrie()
 
 for file in files:
     logging.info(f"Loading {file}")
     with open(path + "/" + file) as f:
         mots = [line.split(";")[0].strip() for line in f.readlines()]
-        dico.update(mots)
+        for mot in mots:
+            dico[mot] = mot
 
 logging.info("Dico loaded")
